8257a4db075be969806a769667c9b5bfc5e5931b
Vlad Bokin <vladislav.bok@yandex.ru>
2024-01-15 11:41:52 +0300

    add icon

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,6 +1,6 @@
 import ProjectDescription
 
 let config = Config(
-    compatibleXcodeVersions: ["14.3.1", "15.0.1"],
+    compatibleXcodeVersions: ["14.3.1", "15.0.1", "15.2"],
     swiftVersion: "5.8.1"
 )
