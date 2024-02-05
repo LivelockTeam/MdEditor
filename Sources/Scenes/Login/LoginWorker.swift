0ce0694b4b414fa9ea1fff9f136a7cd7a7ae80a2
Sergey Rumyantsev <rumyantsev.serge@yandex.ru>
2024-02-05 21:34:37 +0300

    pass fix

--- Sources/Scenes/Login/LoginWorker.swift
+++ Sources/Scenes/Login/LoginWorker.swift
@@ -29,7 +29,7 @@ final class LoginWorker: ILoginWorker {
 	// MARK: - Private properties
 
 	private let validLogin = "Admin"
-	private let validPassword = "pa$$321"
+	private let validPassword = "pa$$32!"
 
 	// MARK: - Public methods
 
